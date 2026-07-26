624f40c24d74087a1041ba0b3b53b7b2ad2e46e4
Jeremy Wu <58065981+jeremywuworldwidesimtan@users.noreply.github.com>
2026-07-26 19:23:52 +0800

    fan sarge

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -3,9 +3,7 @@ import { PrismaPg } from "@prisma/adapter-pg";
 const globalForPrisma = global as unknown as {
   prisma: PrismaClient; 
 }; 
-const adapter = new PrismaPg({
-  connectionString: process.env.DATABASE_URL, 
-}); 
+const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL });
 const prisma =
   globalForPrisma.prisma ||
   new PrismaClient({
